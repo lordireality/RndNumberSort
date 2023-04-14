c0b20220b8d25eed2a023a080298803521ae454f
lordireality <lordireality@gmail.com>
2023-04-15 01:12:54 +0300

    Доделана валидация + небольшой рефактор

--- RndNumberSort/Program.cs
+++ RndNumberSort/Program.cs
@@ -12,10 +12,7 @@ namespace RndNumberSort
 {
     internal class Program
     {
-        /// <summary>
-        /// "магические" размеры массивов из задания
-        /// </summary>
-        public static int[] _possibleSizes = new int[] { 12, 24, 48, 96, 192, 384, 768 };
+
 
         /// <summary>
         /// Основной метод работы программы
@@ -27,42 +24,44 @@ namespace RndNumberSort
             {
                 Console.WriteLine(getCredentials());
                 Console.WriteLine(getPrettyBox("Выберите тип для сортировки: 1 - Вставка, 2 - Отбор"));
-                InputController("Ваш выбор:",new Dictionary<string, string>() { { "typeof","int" }, { "minVal","0" }, { "maxVal","3" } });
+                var SortTypeChoice = InputController("Ваш выбор:",new Dictionary<string, string>() { { "typeof","int" }, { "minVal","0" }, { "maxVal","3" } });
 
                 Console.ReadLine();
             }
         }
 
-
-        public static void InputController(string promt, Dictionary<string,string> validationRules)
+        /// <summary>
+        /// Контроллер ввода значений
+        /// </summary>
+        /// <param name="promt">Текст подсказки</param>
+        /// <param name="validationRules">Правила валидации</param>
+        /// <returns></returns>
+        public static KeyBoardInput_DTO InputController(string promt, Dictionary<string,string> validationRules)
         {
             Console.Write(promt);
             string input = Console.ReadLine();
             var validatorInstance = new Validator(input, validationRules);
-            
-            return;
-        }
-
-
-
-
-        /// <summary>
-        /// Получить массив необходимого размера
-        /// </summary>
-        /// <param name="sizeOfArray">Размер массива - (0 < размер)</param>
-        /// <param name="floor">Мин. значение генерируемых чисел</param>
-        /// <param name="limit">Макс. значение генерируемых чисел</param>
-        /// <returns>Сгенерированных массив</returns>
-        public static int[] getArray(int sizeOfArray, int floor = -100, int limit=100)
-        {
-            var _rndGenerator = new Random();
-            var buff = new int[sizeOfArray];
-            for(int i =0;i<sizeOfArray;i++)
+            if (!validatorInstance.isValid())
             {
-                buff[i]= _rndGenerator.Next(floor, limit);
+                foreach(var message in validatorInstance.messages)
+                {
+                    Console.WriteLine(message);
+                }
+                
             }
-            return buff;
+            var keyBoardInput_DTO = new KeyBoardInput_DTO()
+            {
+                succed = validatorInstance.isValid(),
+                value = input
+            };
+            return keyBoardInput_DTO;
         }
+
+
+
+
+
+
 
         /// <summary>
         /// Копирайт работы в CMD
@@ -93,6 +92,59 @@ namespace RndNumberSort
         }
     }
 
+    public class MagicStuffClass
+    {        
+        /// <summary>
+        /// "магические" размеры массивов из задания
+        /// </summary>
+        public static int[] _possibleSizes = new int[] { 12, 24, 48, 96, 192, 384, 768 };
+
+        /// <summary>
+        /// Получить массив с размером из "магических массивов", по его идентификатору
+        /// </summary>
+        /// <param name="idOfArray">Идентификатор "магического" размера</param>
+        /// <param name="floor">Мин. значение генерируемых чисел</param>
+        /// <param name="limit">Макс. значение генерируемых чисел</param>
+        /// <returns></returns>
+        public static int[] getMagicArray(int idOfArray, int floor = -100, int limit = 100)
+        {
+            return getArray(_possibleSizes[idOfArray], floor, limit);
+        }
+
+        /// <summary>
+        /// Получить массив необходимого размера
+        /// </summary>
+        /// <param name="sizeOfArray">Размер массива - (0 < размер)</param>
+        /// <param name="floor">Мин. значение генерируемых чисел</param>
+        /// <param name="limit">Макс. значение генерируемых чисел</param>
+        /// <returns>Сгенерированных массив</returns>
+        public static int[] getArray(int sizeOfArray, int floor = -100, int limit = 100)
+        {
+            var _rndGenerator = new Random();
+            var buff = new int[sizeOfArray];
+            for (int i = 0; i < sizeOfArray; i++)
+            {
+                buff[i] = _rndGenerator.Next(floor, limit);
+            }
+            return buff;
+        }
+    }
+
+    /// <summary>
+    /// Класс для хранения и передачи информации о вводе с клавиатуры и его успешности
+    /// </summary>
+    public class KeyBoardInput_DTO
+    {
+        /// <summary>
+        /// Успешность ввода
+        /// </summary>
+        public bool succed = false;
+        /// <summary>
+        /// Введенное значение
+        /// </summary>
+        public string value = "";
+    }
+
     /// <summary>
     /// Класс - валидатор для вводимых значений
     /// </summary>
@@ -109,7 +161,7 @@ namespace RndNumberSort
         /// <summary>
         /// Сообщения об ошибках валидации, если такие имеются
         /// </summary>
-        public string[] messages;
+        public List<string> messages = new List<string>();
         /// <summary>
         /// Является ли значение валидным
         /// </summary>
@@ -126,6 +178,14 @@ namespace RndNumberSort
             this.validationRules = validationRules;
             this.Validate();
         }
+        /// <summary>
+        /// Возвращает значение валидации
+        /// </summary>
+        /// <returns>True/False В зависимости от валидности значений</returns>
+        public bool isValid()
+        {
+            return this.valid;
+        }
         private void Validate()
         {
             foreach (var rule in this.validationRules)
@@ -133,12 +193,17 @@ namespace RndNumberSort
                 switch (rule.Key)
                 {
                     case "typeof": valid = ((typeOfCast(rule.Value) == false) ? false : valid);  break;
-                    case "minVal": break; //доделать
-                    case "maxVal": break; //доделать
+                    case "minVal": valid = ((minValCheck(rule.Value) == false) ? false : valid); break;
+                    case "maxVal": valid = ((maxValCheck(rule.Value) == false) ? false : valid); break; 
                     default: Console.Write("[Ошибка!]Незивестное правило валидации - " + rule.Key); break;
                 }
             }
         }
+        /// <summary>
+        /// Валидация типа данных
+        /// </summary>
+        /// <param name="type">Тип</param>
+        /// <returns>true/false в зависимости от успешности валидации</returns>
         private bool typeOfCast(string type)
         {
             int intVal = 0; // костыль для TryParse
@@ -148,6 +213,66 @@ namespace RndNumberSort
                 case "string": return true; //Емаё ввод с клавы и так стринг, костыль
                 default: return false; //не знаем что за тип, возвращаем нет 
             }
+        }
+        /// <summary>
+        /// Валидация "пола" Int32 значений
+        /// </summary>
+        /// <param name="valToCheck">Сравниваемое значение</param>
+        /// <returns>true/false в зависимости от успешности валидации</returns>
+        private bool minValCheck(string valToCheck)
+        {
+            int intVal = 0;
+            int checkVal = 0;
+            Int32.TryParse(valToCheck, out checkVal);
+            bool casted = Int32.TryParse(this.strToValidate, out intVal);
+            
+            if (casted)
+            {
+                if(checkVal < intVal)
+                {
+                    return true;
+                } else
+                {
+                    this.messages.Add("Введенное значение находилось в недопустимом диапазоне!");
+                    return false;
+                }
+            } else
+            {
+                this.messages.Add("Введенное значение имеет недопустимый тип!");
+                return false;
+            }
+            
+        }
+        /// <summary>
+        /// Валидация "потолка" Int32 значений
+        /// </summary>
+        /// <param name="valToCheck">Сравниваемое значение</param>
+        /// <returns>true/false в зависимости от успешности валидации</returns>
+        private bool maxValCheck(string valToCheck)
+        {
+            int intVal = 0;
+            int checkVal = 0;
+            Int32.TryParse(valToCheck, out checkVal);
+            bool casted = Int32.TryParse(this.strToValidate, out intVal);
+
+            if (casted)
+            {
+                if (checkVal > intVal)
+                {
+                    return true;
+                }
+                else
+                {
+                    this.messages.Add("Введенное значение находилось в недопустимом диапазоне!");
+                    return false;
+                }
+            }
+            else
+            {
+                this.messages.Add("Введенное значение имеет недопустимый тип!");
+                return false;
+            }
+
         }
     }
 
